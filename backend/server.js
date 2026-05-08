cc9a6d2e4bf8e9fbe2a38f8285ff3ef90845758d
palem kundhanasree <palemkundhanasree2008@gmail.com>
2026-05-08 18:55:04 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const authRoutes = require('./routes/auth');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 // Middleware
 app.use(cors());
