499b4f0e9691a4046220f26d3ba0873ca5d4b9a5
kumbakonam Supraja <suprajasudhakar109@gmail.com>
2026-05-10 22:49:47 +0530

    fetch data from db

--- backend/routes/report.js
+++ backend/routes/report.js
@@ -17,5 +17,18 @@ router.post('/add', async (req, res) => {
         res.status(500).json({ message: 'Failed to submit report', error });
     }   
 });
+router.get("/reports/:userId", async (req, res) => {
+    try {
+        const reports = await Report.find({
+            userId: req.params.userId
+        });
+        res.status(200).json(reports);
+    }
+    catch(error){
+        res.status(500).json({
+            message: "Failed to fetch reports"
+        });
+    }
+});
 
 module.exports= router;
