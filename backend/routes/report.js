c1ca05495c5302ef25d0a4f06459a2defd4a7aea
palem kundhanasree <palemkundhanasree2008@gmail.com>
2026-05-12 19:31:29 +0530

    created route for report feed

--- backend/routes/report.js
+++ backend/routes/report.js
@@ -47,4 +47,16 @@ router.get('/user/:userId', async (req, res) => {
         res.status(500).json({ message: 'Failed to fetch reports', error: error.message });
     }
 });
+
+router.get('/all', async (req, res) => {
+    try{
+        const reports = await Report.find().sort({ createdAt: -1 });
+        res.status(200).json(reports);
+    }
+    catch(error){
+        console.error('Error fetching reports:', error);
+        res.status(500).json({ message: 'Failed to fetch reports', error: error.message });
+    }
+});
+
 module.exports= router;
