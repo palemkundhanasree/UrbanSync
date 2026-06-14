7b13dacb2fa16430356a85b500cf4c9f05b85f89
palem kundhanasree <palemkundhanasree2008@gmail.com>
2026-06-14 15:55:34 +0530

    ..

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,7 +6,9 @@ const nodemailer = require("nodemailer");
 const User = require('../models/UserSchema');
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    secure: process.env.EMAIL_SECURE === 'true', // true for 465, false for other ports
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
