644d5025ce2333afec0f6136580ca09a573c7f82
palem kundhanasree <palemkundhanasree2008@gmail.com>
2026-06-14 14:27:26 +0530

    updated

--- backend/routes/otp.js
+++ backend/routes/otp.js
@@ -6,10 +6,15 @@ const User = require('../models/UserSchema');
 const OTP = require('../models/OTPSchema');   
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    secure: process.env.EMAIL_SECURE === 'true',
     auth: {
         user: process.env.EMAIL_USER, 
         pass: process.env.EMAIL_PASS  
+    },
+    tls: {
+        rejectUnauthorized: false
     }
 });
 
