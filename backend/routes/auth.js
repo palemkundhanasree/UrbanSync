12593c13679123a373b164ad3142003ff9c14869
palem kundhanasree <palemkundhanasree2008@gmail.com>
2026-06-14 16:15:50 +0530

    Hardcode Gmail IPv4 address to fix ENETUNREACH

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -8,14 +8,14 @@ const User = require('../models/UserSchema');
 const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
-    secure: process.env.EMAIL_SECURE === 'true', // true for 465, false for other ports
-    lookup: (hostname, options, callback) => {
-        options.family = 4; // Forces IPv4 resolution
-        return dns.lookup(hostname, options, callback);
-    },
+    secure: process.env.EMAIL_SECURE === 'true',
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
+    },
+    tls: {
+        servername: 'smtp.gmail.com',
+        rejectUnauthorized: false
     }
 });
 
